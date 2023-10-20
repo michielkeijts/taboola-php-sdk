fcc037d7437c5513f4611031dd0ea8b4e885e1c5
Michiel Keijts <michiel@normit.nl>
2023-10-20 16:01:22 +0200

    Update

--- src/Client/Requests/TaboolaCampaignRequest.php
+++ src/Client/Requests/TaboolaCampaignRequest.php
@@ -1,8 +1,8 @@
 <?php
-/* 
+/*
  * @copyright (C) 2020 Michiel Keijts, Normit
- * 
- * 
+ *
+ *
  * @licence MIT
  */
 
@@ -13,43 +13,43 @@ use TaboolaApi\Client\Requests\BaseRequest;
 class TaboolaCampaignRequest extends BaseRequest {
     /**
      * The Endpoint URL
-     * @var string 
+     * @var string
      */
     protected $endpoint = "%s/campaigns/";
-    
-    public function getCampaigns(string $account_id) 
+
+    public function getCampaigns(string $account_id)
     {
         return $this->request('get',"", sprintf($this->endpoint, $account_id));
     }
-    
-    public function getAllCampaigns(string $account_id, int $page = 1, int $page_size = 100) 
+
+    public function getAllCampaigns(string $account_id, int $page = 1, int $page_size = 100)
     {
         return $this->request('get',['page'=>$page, 'page_size'=>$page_size], sprintf($this->endpoint . 'base', $account_id));
     }
-    
-    public function getCampaign(string $account_id, int $id) 
+
+    public function getCampaign(string $account_id, int $id)
     {
         return $this->request('get',"", sprintf($this->endpoint. '%d', $account_id, $id));
     }
-    
-    public function createCampaign(string $account_id, array $data) 
+
+    public function createCampaign(string $account_id, array $data)
     {
         return $this->request('post', $data, sprintf($this->endpoint, $account_id));
     }
-    
-    public function updateCampaign(string $account_id, string $id, array $data) 
+
+    public function updateCampaign(string $account_id, string $id, array $data)
     {
         return $this->request('post', $data, sprintf($this->endpoint . DS . '%s', $account_id, $id));
     }
-    
+
     public function deleteCampaign(string $account_id, string $campaign_id)
     {
         return $this->request('delete', "", sprintf($this->endpoint . DS . '%s', $account_id, $campaign_id));
     }
-    
-    public function duplicateCampaign(string $account_id, string $campaign_id, array $data = [], string $destination_advertiser_id = "") 
+
+    public function duplicateCampaign(string $account_id, string $campaign_id, array $data = [], string $destination_advertiser_id = "")
     {
-        // see https://developers.taboola.com/backstage-api/reference#duplicate-a-campaign if 
+        // see https://developers.taboola.com/backstage-api/reference#duplicate-a-campaign if
         // duplicating to different advertiser, use query parameter
         if (!empty($destination_advertiser_id) && $destination_advertiser_id  !== $account_id) {
             $destination_advertiser_id = '?destination_account=' . $destination_advertiser_id;
@@ -58,27 +58,43 @@ class TaboolaCampaignRequest extends BaseRequest {
         }
         return $this->request('post', $data, sprintf($this->endpoint .  '%s/duplicate%s', $account_id, $campaign_id, $destination_advertiser_id));
     }
-    
+
     public function getCampaignItems(string $account_id, string $campaign_id)
     {
         return $this->request('get', "", sprintf($this->endpoint . DS . '%s/items', $account_id, $campaign_id));
     }
-    
+
     public function getCampaignItem(string $account_id, string $campaign_id, string $campaign_item_id)
     {
         return $this->request('get', "", sprintf($this->endpoint . DS . '%s/items/%s', $account_id, $campaign_id, $campaign_item_id));
     }
-    
+
     public function createCampaignItem(string $account_id, string $campaign_id, array $data)
     {
         return $this->request('post', $data, sprintf($this->endpoint . DS . '%s/items', $account_id, $campaign_id));
     }
-    
+
+    /**
+     * Mass upate, $data['collection'=>[..items..]
+     *
+     * @param string $account_id
+     * @param string $campaign_id
+     * @param array $data
+     * @return \TaboolaApi\Client\Responses\Response
+     */
+    public function createCampaignItems(string $account_id, string $campaign_id, array $data)
+    {
+        if (!array_key_exists('collection', $data)) {
+            $data = ['collection'=>$data];
+        }
+        return $this->request('post', $data, sprintf($this->endpoint . DS . '%s/items/mass', $account_id, $campaign_id));
+    }
+
     public function updateCampaignItem(string $account_id, string $campaign_id, string $item_id, array $data)
     {
         return $this->request('post', $data, sprintf($this->endpoint . DS . '%s/items/%s', $account_id, $campaign_id, $item_id));
     }
-    
+
     public function deleteCampaignItem(string $account_id, string $campaign_id, string $item_id)
     {
         return $this->request('delete', "", sprintf($this->endpoint . DS . '%s/items/%s', $account_id, $campaign_id, $item_id));
